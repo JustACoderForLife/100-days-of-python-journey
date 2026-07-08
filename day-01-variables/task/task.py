f16d51743991ae62122de1784cd0daea52451210
JustACoderForLife <vivaangarg71@gmail.com>
2026-07-08 18:38:27 +0530

    Add Python course projects for Days 1 through 5
    Uploading initial bootcamp progress, including variables, data types, control flow, lists, loops, and the PyPassword Generator.

---
+++ day-01-variables/task/task.py
@@ -0,0 +1,4 @@
+print("Hello, Dear " + input("What is your name?\n")+ ". Hope your day is going well." )
+city = input("What was the city that you grew up in?\n")
+pet_name = input("What is your pet's name?\n")
+print("A possible Band Name might be:", city + " " + pet_name)
